2eaeed63c290fe944b7082c5a9df953084abc6ad
abiwooli <abdiwolih@gmail.com>
2024-02-24 13:03:47 +0300

    uncomment

--- api/v1/app.py
+++ api/v1/app.py
@@ -18,6 +18,6 @@ def not_found(error):
     return jsonify({"error": "Not found"}), 404
 
 if __name__ == "__main__":
-    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', 5000))
-    app.run(host=host, port=port, threaded=True)
+    #host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    #port = int(os.getenv('HBNB_API_PORT', 5000))
+    app.run(host='0.0.0.0', port=5000, threaded=True)
