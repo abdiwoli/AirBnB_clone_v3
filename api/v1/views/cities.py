4f7b35dca902c2e1b911da1c3bf32a88a36dfee5
Muna Saeed <munasaeed6367@gmail.com>
2024-02-25 02:22:38 +0300

    6 n 8

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,7 +5,7 @@ from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models.city import City
 from models.state import State
-
+from models.amenities import Amenities
 
 @app_views.route('/states/<sid>/cities', methods=['GET'], strict_slashes=False)
 def ret_cities(sid):
